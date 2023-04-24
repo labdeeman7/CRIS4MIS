ce1e56b9ca15d9d10e7f8ec55df3ddc92f37faa8
Oluwatosin Alabi <k21168006@erc-hpc-login1.create.kcl.ac.uk>
2023-04-24 12:15:20 +0100

    moving to dgx create not working hahahaha

--- shell_create_train_kcl_tosin.sh
+++ shell_create_train_kcl_tosin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
-#SBATCH --output=/scratch/users/%u/output_terminal/or_unet_%j.out
+#SBATCH --output=/scratch/users/%u/output_terminal/cris_%j.out
 #SBATCH --job-name=cris4mis
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=7
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem-per-cpu=262144
