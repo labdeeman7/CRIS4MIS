ffe162e04aba0d9b9bf713bbb7de0ae9a7fd30e8
Zijian Zhou <franciszzj@gmail.com>
2023-04-28 22:12:28 +0100

    update evaluate

--- evaluate_2017.py
+++ evaluate_2017.py
@@ -12,24 +12,6 @@ binary_factor = 255
 parts_factor = 85
 instruments_factor = 32
 
-seq2path = {
-    '1': '../train_val/miccai_challenge_2018_release_1/seq_1/labels',
-    '2': '../train_val/miccai_challenge_2018_release_1/seq_2/labels',
-    '3': '../train_val/miccai_challenge_2018_release_1/seq_3/labels',
-    '4': '../train_val/miccai_challenge_2018_release_1/seq_4/labels',
-    '5': '../train_val/miccai_challenge_release_2/seq_5/labels',
-    '6': '../train_val/miccai_challenge_release_2/seq_6/labels',
-    '7': '../train_val/miccai_challenge_release_2/seq_7/labels',
-    '9': '../train_val/miccai_challenge_release_3/seq_9/labels',
-    '10': '../train_val/miccai_challenge_release_3/seq_10/labels',
-    '11': '../train_val/miccai_challenge_release_3/seq_11/labels',
-    '12': '../train_val/miccai_challenge_release_3/seq_12/labels',
-    '13': '../train_val/miccai_challenge_release_4/seq_13/labels',
-    '14': '../train_val/miccai_challenge_release_4/seq_14/labels',
-    '15': '../train_val/miccai_challenge_release_4/seq_15/labels',
-    '16': '../train_val/miccai_challenge_release_4/seq_16/labels',
-}
-
 
 def general_jaccard(y_true, y_pred):
     result = []
@@ -76,15 +58,6 @@ def dice(y_true, y_pred):
                                                     y_pred.sum() + 1e-15)
 
 
-def rgb2id(color):
-    if isinstance(color, np.ndarray) and len(color.shape) == 3:
-        if color.dtype == np.uint8:
-            color = color.astype(np.int32)
-        return color[:, :,
-                     0] + 256 * color[:, :, 1] + 256 * 256 * color[:, :, 2]
-    return int(color[0] + 256 * color[1] + 256 * 256 * color[2])
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
@@ -92,12 +65,12 @@ if __name__ == '__main__':
     arg('--test_path',
         type=str,
         default=
-        '/jmain02/home/J2AD019/exk01/zxz35-exk01/data/cambridge-1/EndoVis2018/val',
+        '/jmain02/home/J2AD019/exk01/zxz35-exk01/data/cambridge-1/EndoVis2017/cropped_test',
         help='path where train images with ground truth are located')
     arg('--pred_path',
         type=str,
         default=
-        '/jmain02/home/J2AD019/exk01/zxz35-exk01/data/cambridge-1/CRIS/exp/endovis2018/v1_0/score',
+        '/jmain02/home/J2AD019/exk01/zxz35-exk01/data/cambridge-1/CRIS/exp/endovis2017/v1_0/score',
         help='path with predictions')
     arg('--problem_type',
         type=str,
@@ -111,19 +84,17 @@ if __name__ == '__main__':
 
     if args.problem_type == 'binary':
         class_name_list = ['background', 'instrument']
+        factor = binary_factor
     elif args.problem_type == 'parts':
         class_name_list = ['background', 'shaft', 'wrist', 'claspers']
+        factor = parts_factor
     elif args.problem_type == 'instruments':
         class_name_list = [
-            'background',
-            'bipolar_forceps',
-            'prograsp_forceps',
-            'large_needle_driver',
-            'monopolar_curved_scissors',
-            'ultrasound_probe',
-            'suction_instrument',
-            'clip_applier',
+            'background', 'bipolar_forceps', 'prograsp_forceps',
+            'large_needle_driver', 'vessel_sealer', 'grasping_retractor',
+            'monopolar_curved_scissors', 'other_medical_instruments'
         ]
+        factor = instruments_factor
 
     # palette
     if args.vis:
@@ -139,78 +110,76 @@ if __name__ == '__main__':
                 palette[i][:, :, j] = palette_list[i][j]
 
     # evaluate
-    image_dir = os.path.join(args.test_path, 'images')
-    for image_file in tqdm(os.listdir(image_dir)):
-        image_id = image_file.split('.')[0]
-        image_path = os.path.join(image_dir, image_file)
-        anno_path = image_path.replace('images', 'annotations')
-        mask = cv2.imread(anno_path)
-
-        # generate mask label
-        if args.problem_type == 'binary':
-            y_true = (mask > 0) * 1
-        elif args.problem_type == 'parts':
-            _, seq_id, image_name = image_file.split('_')
-            parts_mask_path = os.path.join(args.test_path, seq2path[seq_id],
-                                           image_name)
-            parts_mask = cv2.imread(parts_mask_path)
-            parts_mask = cv2.cvtColor(parts_mask, cv2.COLOR_BGR2RGB)
-            parts_mask = rgb2id(parts_mask)
-            y_true = np.zeros(parts_mask.shape)
-            for class_name in ['shaft', 'wrist', 'claspers']:
-                if class_name == 'shaft':
-                    y_true += (parts_mask
-                               == (125 + 255 * 256 + 12 * 256 * 256)) * 1
-                elif class_name == 'wrist':
-                    y_true += (parts_mask
-                               == (0 + 255 * 256 + 0 * 256 * 256)) * 2
-                elif class_name == 'claspers':
-                    y_true += (parts_mask
-                               == (0 + 255 * 256 + 255 * 256 * 256)) * 3
-        elif args.problem_type == 'instruments':
-            y_true = mask
-
-        # vis
+    if 'train' in args.test_path:
+        dataset_num = 8
+    elif 'test' in args.test_path:
+        dataset_num = 10
+    for instrument_id in range(1, dataset_num + 1):
+        instrument_dataset_name = 'instrument_dataset_{}'.format(instrument_id)
+        file_dir = os.path.join(args.test_path, instrument_dataset_name,
+                                '{}_masks'.format(args.problem_type))
         if args.vis:
-            image = cv2.imread(image_path)
-            gt_mask = y_true
-            show = np.zeros_like(image)
-            for i_h in range(height):
-                for i_w in range(width):
-                    show[i_h, i_w] = palette[gt_mask[i_h, i_w], i_h, i_w]
-            gt_vis_image = image * 0.5 + show * 0.5
+            image_dir = os.path.join(args.test_path, instrument_dataset_name,
+                                     'images')
+        for file_name in tqdm(os.listdir(file_dir),
+                              desc=instrument_dataset_name):
+            file_id = file_name.split('.')[0]
 
-        image_split = '_'.join(image_file.split('_')[:2])
-        file_id = image_file.split('_')[2]
-        pred_image_list = []
-        for class_name in class_name_list:
-            pred_file_name = os.path.join(
-                args.pred_path,
-                'score-{}-{}-{}.npz'.format(image_split, file_id, class_name))
-            if os.path.exists(pred_file_name):
-                pred_dict = np.load(pred_file_name)
-                pred_image = cv2.warpAffine(pred_dict.get('pred'),
-                                            pred_dict.get('mat'),
-                                            (width, height),
-                                            flags=cv2.INTER_CUBIC,
-                                            borderValue=0.)
-            else:
-                pred_image = np.zeros_like(y_true)
-            pred_image_list.append(pred_image)
-        pred_image = np.array(pred_image_list)
-        y_pred = np.argmax(pred_image, axis=0)
+            file_path = os.path.join(file_dir, file_name)
+            y_true = cv2.imread(file_path, 0).astype(np.uint8)
 
-        if args.vis:
-            show = np.zeros_like(image)
-            for i_h in range(height):
-                for i_w in range(width):
-                    show[i_h, i_w] = palette[y_pred[i_h, i_w], i_h, i_w]
-            pred_vis_image = image * 0.5 + show * 0.5
+            if args.vis:
+                if 'cropped_train' in args.test_path:
+                    image_path = os.path.join(image_dir,
+                                              '{}.jpg'.format(file_id))
+                elif 'cropped_test' in args.test_path:
+                    image_path = os.path.join(image_dir,
+                                              '{}.png'.format(file_id))
+                image = cv2.imread(image_path)
 
-            vis_image = np.concatenate([gt_vis_image, pred_vis_image], axis=1)
-            cv2.imwrite(
-                '{}/{}_{}_{}.jpg'.format(eval_dir, args.problem_type,
-                                         image_split, file_id), vis_image)
+                gt_mask = y_true // factor
+                show = np.zeros_like(image)
+                for i_h in range(height):
+                    for i_w in range(width):
+                        show[i_h, i_w] = palette[gt_mask[i_h, i_w], i_h, i_w]
+                # show = np.take(palette, gt_mask)
+                gt_vis_image = image * 0.5 + show * 0.5
+
+            pred_image_list = []
+            for class_name in class_name_list:
+                pred_file_name = os.path.join(
+                    args.pred_path,
+                    'score-{}-{}-{}.npz'.format(instrument_dataset_name,
+                                                file_id, class_name))
+                if os.path.exists(pred_file_name):
+                    pred_dict = np.load(pred_file_name)
+                    pred_image = cv2.warpAffine(pred_dict.get('pred'),
+                                                pred_dict.get('mat'),
+                                                (width, height),
+                                                flags=cv2.INTER_CUBIC,
+                                                borderValue=0.)
+                else:
+                    pred_image = np.zeros_like(y_true)
+                pred_image_list.append(pred_image)
+            pred_image = np.array(pred_image_list)
+            y_pred = np.argmax(pred_image, axis=0)
+
+            if args.vis:
+                show = np.zeros_like(image)
+                for i_h in range(height):
+                    for i_w in range(width):
+                        show[i_h, i_w] = palette[y_pred[i_h, i_w], i_h, i_w]
+                pred_vis_image = image * 0.5 + show * 0.5
+
+                vis_image = np.concatenate([gt_vis_image, pred_vis_image],
+                                           axis=1)
+                cv2.imwrite(
+                    '{}/{}_{}_{}.jpg'.format(eval_dir, args.problem_type,
+                                             instrument_dataset_name, file_id),
+                    vis_image)
+
+            y_pred = y_pred * factor
+            y_pred = y_pred.astype(np.uint8)
 
             result_jaccard += [general_jaccard(y_true, y_pred)]
             result_dice += [general_dice(y_true, y_pred)]
